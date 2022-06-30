9370aacc4477e1d703a1fb2ed16cceb38d93fddd
Tony K <tonykha.1705@gmail.com>
2022-06-30 19:02:46 +1200

    setup more routes and db function boiletplate

--- server/routes/itemsRoutes.js
+++ server/routes/itemsRoutes.js
@@ -3,3 +3,5 @@ const express = require('express')
 const db = require('../db/itemsFunc')
 
 const router = express.Router()
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 
 const fruitRoutes = require('./routes/fruits')
+const itemRoutes = require('./routes/itemsRoutes')
+const reviewRoutes = require('./routes/reviewRoutes')
 
 const server = express()
 
@@ -9,5 +11,10 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/fruits', fruitRoutes)
+// //prefix for item routes
+// server.use('/api/v1/items', itemRoutes)
+
+// //prefix for item routes
+// server.use('/api/v1/items', reviewRoutes)
 
 module.exports = server
