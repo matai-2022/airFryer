d1f4169ed90da520289a6f65077ffcae3d60df7b
Saia <makasiosaia@gmail.com>
2022-07-01 11:19:14 +1200

    create navigation function in tile components

--- client/components/Tile.jsx
+++ client/components/Tile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 function Tile(props) {
   return (
     <>
-      <section className="tile-wrapper">
+      <section className="tile-wrapper" onClick={props.itemHandler}>
         <h2 className="tile-title">{props.titleName}</h2>
       </section>
     </>
