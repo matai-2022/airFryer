89b2070d7c54d989e4d56c1fcc573447eb858069
Tony K <tonykha.1705@gmail.com>
2022-06-30 18:52:17 +1200

    seed done

--- server/db/seeds/items.js
+++ server/db/seeds/items.js
@@ -1,13 +1,10 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
-};
+  await knex('items').del()
+  await knex('items').insert([
+    { id: 1, item_name: "Phu's chilli oil", contributor_name: 'Phu' },
+    { id: 2, item_name: 'Crew board game', contributor_name: 'Phu' },
+    { id: 3, item_name: 'Air Fryer', contributor_name: 'Tony' },
+    { id: 4, item_name: 'Hot Pot', contributor_name: 'JingJing' },
+  ])
+}
--- server/db/seeds/reviews.js
+++ server/db/seeds/reviews.js
@@ -1,13 +1,34 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    { id: 1, colName: 'rowValue1' },
-    { id: 2, colName: 'rowValue2' },
-    { id: 3, colName: 'rowValue3' },
+  await knex('reviews').del()
+  await knex('reviews').insert([
+    {
+      id: 1,
+      item_id: 1,
+      reviewer_name: 'Liam',
+      review: 'More pls',
+      profile_image: 'image_url.jpg',
+    },
+    {
+      id: 2,
+      item_id: 1,
+      reviewer_name: 'Tony',
+      review: 'More pls',
+      profile_image: 'image_url.jpg',
+    },
+    {
+      id: 3,
+      item_id: 3,
+      reviewer_name: 'Saia',
+      review: 'Your air fryer smells',
+      profile_image: 'image_url.jpg',
+    },
+    {
+      id: 4,
+      item_id: 2,
+      reviewer_name: 'Tony',
+      review: 'This game gives me so much anxiety ',
+      profile_image: 'image_url.jpg',
+    },
   ])
 }
