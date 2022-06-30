bd6c2d2683c393c5f39cb6e0f9e6cd9b2cfddf49
saia-maka <makasiosaia@gmail.com>
2022-07-01 07:47:23 +1200

    create review input component

---
+++ client/components/ReviewInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ReviewInput() {
+  return <>
+  <section className='review-input-wrapper'>
+    <input className='review-input' />
+    <button className='review-input-btn'>
+      Add review
+    </button>
+  </section>
+  </>
+}
+
+export default ReviewInput
