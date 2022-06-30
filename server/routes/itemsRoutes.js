ec1a51a18084a555f615c70628e62d797ef2d371
Tony K <tonykha.1705@gmail.com>
2022-06-30 19:12:35 +1200

    setup for get and post routes for both review and items

--- server/routes/itemsRoutes.js
+++ server/routes/itemsRoutes.js
@@ -4,4 +4,23 @@ const db = require('../db/itemsFunc')
 
 const router = express.Router()
 
-module.exports = router
+router.get('/', async (req, res) => {
+  try {
+    // const items = await db function goes here
+    // res.json (items)
+    // return null
+  } catch (error) {
+    res.status(500).send('DATABASE ERROR: ' + error.message)
+  }
+})
+
+// router.post('/', async (req, res) => {
+//   try {
+//     const items =req.body
+//     await db function goes here pass through items
+//     res.sendStatus(201)
+//     return null
+//   } catch (error) {
+//     res.status(500).send('DATABASE ERROR: ' + error.message)
+//   }
+// })
--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -4,4 +4,25 @@ const db = require('../db/reviewsFunc')
 
 const router = express.Router()
 
+router.get('/', async (req, res) => {
+  try {
+    // const review = await db function goes here
+    // res.json (review)
+    // return null
+  } catch (error) {
+    res.status(500).send('DATABASE ERROR: ' + error.message)
+  }
+})
+
+// router.post('/', async (req, res) => {
+//   try {
+//     const reviews =req.body
+//     await db function goes here pass through items
+//     res.sendStatus(201)
+//     return null
+//   } catch (error) {
+//     res.status(500).send('DATABASE ERROR: ' + error.message)
+//   }
+// })
+
 module.exports = router
