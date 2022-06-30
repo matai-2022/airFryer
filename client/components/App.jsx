f2e55197219507e2c2cac8594fa2089be60ed19f
saia-maka <makasiosaia@gmail.com>
2022-07-01 00:30:43 +1200

    edit item/review styling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -20,8 +20,8 @@ function App() {
     <>
       <div className="app">
         <Header />
-        <TileList />
-        {/* <Item /> */}
+        {/* <TileList /> */}
+        <Item />
       </div>
     </>
   )
