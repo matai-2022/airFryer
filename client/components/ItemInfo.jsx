c7b2586a4304a3316ed18c3cf764496cf659c6b8
Saia <makasiosaia@gmail.com>
2022-07-01 12:18:37 +1200

    connect slice data and render on item/review components

--- client/components/ItemInfo.jsx
+++ client/components/ItemInfo.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-
-const testData = {
-  id: 1,
-  item_name: "Phu's chilli oil",
-  contributor_name: 'Phu',
-}
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchItems } from '../slices/items'
 
 function ItemInfo() {
   const { id } = useParams()
+  const itemData = useSelector((state) => state.items)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchItems())
+  }, [])
+
+  const item = itemData.find((item) => item.id === Number(id))
 
   return (
     <>
       <section className="item-info-wrapper">
-        <h2 className="item-title">{testData.item_name}</h2>
-        <h3 className="item-owner-name">By {testData.contributor_name}</h3>
+        <h2 className="item-title">{item.item_name}</h2>
+        <h3 className="item-owner-name">By {item.contributor_name}</h3>
       </section>
     </>
   )
--- client/components/ReviewList.jsx
+++ client/components/ReviewList.jsx
@@ -1,66 +1,33 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-import { selectItemReviews, fetchItemReviews } from '../slices/reviews'
+import { fetchItemReviews } from '../slices/reviews'
 
 // import components:
 import Review from './Review'
 import ReviewInput from './ReviewInput'
 
-const testData = [
-  {
-    id: 1,
-    item_id: 1,
-    reviewer_name: 'Liam',
-    review: 'More pls',
-    profile_image: '/images/profile.png',
-  },
-  {
-    id: 2,
-    item_id: 1,
-    reviewer_name: 'Tony',
-    review: 'More pls',
-    profile_image: '/images/profile.png',
-  },
-  {
-    id: 3,
-    item_id: 3,
-    reviewer_name: 'Saia',
-    review: 'Your air fryer smells',
-    profile_image: '/images/profile.png',
-  },
-  {
-    id: 4,
-    item_id: 2,
-    reviewer_name: 'Tony',
-    review: 'This game gives me so much anxiety ',
-    profile_image: '/images/profile.png',
-  },
-]
-
 function ReviewList() {
   const { id } = useParams()
-  console.log(id, ' this is id')
+
   const dispatch = useDispatch()
-  const reviewData = useSelector()
+  const reviewData = useSelector((state) => state.reviews)
   useEffect(async () => {
     dispatch(fetchItemReviews(id))
   }, [])
-  console.log(reviewData, 'this is reviewData')
 
   return (
     <>
       <main className="review-list-wrapper">
         <h2 className="review-list-title">Reviews .</h2>
         <ReviewInput />
-        {testData.map((review) => {
+        {reviewData.map((review) => {
           return (
             <Review
               key={review.id}
               name={review.reviewer_name}
               review={review.review}
               profileImg={review.profile_image}
-              // dataId={id}
             />
           )
         })}
