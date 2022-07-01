751d0a7b66fbea2d7deb2d14ab6e33ecd711903a
Christelle Quilang <christelle.quilang@yahoo.co.nz>
2022-07-01 13:54:41 +1200

    Add delete button

--- client/components/ItemInfo.jsx
+++ client/components/ItemInfo.jsx
@@ -4,10 +4,6 @@ import { useSelector, useDispatch } from 'react-redux'
 import { fetchItems } from '../slices/items'
 
 function ItemInfo() {
-  function handleDelete() {
-    alert('No takebacks!')
-  }
-
   const { id } = useParams()
   const itemData = useSelector((state) => state.items)
   const dispatch = useDispatch()
@@ -23,9 +19,6 @@ function ItemInfo() {
       <section className="item-info-wrapper">
         <h2 className="item-title">{item.item_name}</h2>
         <h3 className="item-owner-name">By {item.contributor_name}</h3>
-        <button type="button" onClick={handleDelete}>
-          Delete
-        </button>
       </section>
     </>
   )
